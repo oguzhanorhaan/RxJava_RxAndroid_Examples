401fec3d77c19c12b539aa930b2e4d2b5b3b1285
oguzhanorhaan <ozy.orhan@gmail.com>
2019-09-06 17:39:33 +0300

    FlatMapOperator example added

---
+++ Operators/app/src/main/java/com/example/operators/Activites/FlatMapActivity.java
@@ -0,0 +1,146 @@
+package com.example.operators.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.operators.Models.User;
+import com.example.operators.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+public class FlatMapActivity extends AppCompatActivity {
+
+    private static final String TAG = FlatMapActivity.class.getSimpleName();
+
+    private Disposable disposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flat_map);
+
+        getUsersObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Function<User, Observable<User>>() {
+
+                    @Override
+                    public Observable<User> apply(User user) throws Exception {
+
+                        // getting each user address by making another network call
+                        return getAddressObservable(user);
+                    }
+                })
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e(TAG, "onSubscribe");
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.e(TAG, "onNext: " + user.getName() + ", " + user.getGender() + ", " + user.getAddress());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "All users emitted!");
+                    }
+                });
+
+
+    }
+
+    /**
+     * Assume this as a network call
+     * returns Users with address filed added
+     */
+    private Observable<User> getAddressObservable(final User user) {
+
+        final String[] addresses = new String[]{
+                "1600 Amphitheatre Parkway, Mountain View, CA 94043",
+                "2300 Traverwood Dr. Ann Arbor, MI 48105",
+                "500 W 2nd St Suite 2900 Austin, TX 78701",
+                "355 Main Street Cambridge, MA 02142"
+        };
+
+        return Observable
+                .create(new ObservableOnSubscribe<User>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<User> emitter) throws Exception {
+                        if (!emitter.isDisposed()) {
+                            user.setAddress(addresses[new Random().nextInt(2) + 0]);
+
+
+                            // Generate network latency of random duration
+                            int sleepTime = new Random().nextInt(1000) + 500;
+
+                            Thread.sleep(sleepTime);
+                            emitter.onNext(user);
+                            emitter.onComplete();
+                        }
+                    }
+                }).subscribeOn(Schedulers.io());
+    }
+
+    /**
+     * Assume this is a network call to fetch users
+     * returns Users with name and gender but missing address
+     */
+    private Observable<User> getUsersObservable() {
+        String[] maleUsers = new String[]{"Mark", "John", "Trump", "Obama"};
+
+        final List<User> users = new ArrayList<>();
+
+        for (String name : maleUsers) {
+            User user = new User();
+            user.setName(name);
+            user.setGender("male");
+
+            users.add(user);
+        }
+
+        return Observable
+                .create(new ObservableOnSubscribe<User>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<User> emitter) throws Exception {
+                        for (User user : users) {
+                            if (!emitter.isDisposed()) {
+                                emitter.onNext(user);
+                            }
+                        }
+
+                        if (!emitter.isDisposed()) {
+                            emitter.onComplete();
+                        }
+                    }
+                }).subscribeOn(Schedulers.io());
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
+    }
+}
---
+++ Operators/app/src/main/java/com/example/operators/Models/User.java
@@ -0,0 +1,31 @@
+package com.example.operators.Models;
+
+public class User {
+    private  String address;
+    private String name;
+    private String gender;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
